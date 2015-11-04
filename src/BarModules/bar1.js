230df59699d441e7a0308c61291fe9035f22b4a5
Francis Paulin <paulinfrancis@gmail.com>
2015-11-04 18:33:53 +0100

    fix typo

--- src/BarModules/bar1.js
+++ src/BarModules/bar1.js
@@ -1,4 +1,4 @@
-//define('bar1', ['BarModules/bar2.js', 'FooModules/foo1.js'], function(bar2, foo1){     Works, but is "wrong"
+//define('bar1', ['BarModules/bar2.js', 'FooModules/foo1.js'], function(bar2, foo1){    // Works, but is "wrong"
 define('bar1', ['BarModules/bar2', 'FooModules/foo1'], function(bar2, foo1){
 	return {
 		hi: function(){
